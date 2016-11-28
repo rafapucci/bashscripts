8a4faa3dff231792a1ce040219de6be71a1c6bfb
rafapucci <rafapucci@gmail.com>
2016-11-28 15:04:06 -0200

    python script to solve tarmful

---
+++ ctf/solve.py
@@ -0,0 +1,19 @@
+import zipfile
+
+def deszipa( local ):
+
+    file = open(local, "rb")
+    z = zipfile.ZipFile(file)
+    nfile = ""
+
+    for name in z.namelist():
+                if ".zip" in name:
+                    print '[DEBUG >>] New zip file: ' + name
+                    z.extract(name, "")
+                    deszipa(name)
+    file.close()
+    return;
+
+
+print "[DEBUG >>] Function zip"
+deszipa("./arq.zip")
